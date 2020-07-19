11d3a8e1f24c6c8f24b335ff73f59f9568b0cbb8
Kunju Perath <kperath@gmail.com>
2020-07-19 15:57:19 -0400

    backing up initialization script

---
+++ db/init.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS user (
+    id integer PRIMARY KEY,
+    username text NOT NULL UNIQUE,
+    password text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS hero (
+    id integer,
+    name text NOT NULL,
+    year integer,
+    info text NOT NULL,
+    user_id integer NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+    PRIMARY KEY (id, user_id)
+);
+
+
+-- adding dummy data
+INSERT INTO user (username, password) VALUES ("manman", "manman");
+INSERT INTO user (username, password) VALUES ("frogboy", "frogboy");
+
+INSERT INTO hero (id, name, year, info, user_id)
+    VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM hero WHERE user_id = 1), "Aquaman", 2000, "king of atlantis", 1);
+INSERT INTO hero (id, name, year, info, user_id)
+    VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM hero WHERE user_id = 1), "Flash", 2000, "Speedster", 1);
+INSERT INTO hero (id, name, year, info, user_id)
+    VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM hero WHERE user_id = 1), "Wonder Woman", 2000, "Queen", 1);
+
+INSERT INTO hero (id, name, year, info, user_id)
+    VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM hero WHERE user_id = 2), "Green Arrow", 1999, "arrow", 2);
+INSERT INTO hero (id, name, year, info, user_id)
+    VALUES ((SELECT IFNULL(max(id), 0) + 1 FROM hero WHERE user_id = 2), "Green Lanturn", 1999, "arrow", 2);
